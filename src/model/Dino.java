8a28dd8ee1f9fa62479278c6773684a8062694b6
yuilsi <yuilsimadeleine@gmail.com>
2021-06-09 11:50:31 -0500

    prueba y anexo de PApplet

--- src/model/Dino.java
+++ src/model/Dino.java
@@ -1,11 +1,12 @@
 package model;
 
-
+import processing.core.PApplet;
 
 public class Dino  extends Caveman {
 
 	private int vx;
 	private int vy;
+	private PApplet app;
 	
 	public Dino(String recurso, int x, int y, int xt,int vx,int vy) {
 		super(recurso, x, y, xt);
